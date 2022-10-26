883b5da0cd3074e1218b60487744704b277a0335
atc19 <atc19@inf.ufpr.br>
2022-10-26 14:28:36 -0300

    email done

---
+++ config.py
@@ -0,0 +1,6 @@
+
+# Main email server config
+host = 'smtp.gmail.com' ### https://support.google.com/accounts/answer/185833
+port = 587
+email = 'coelhoarturdev@gmail.com'
+password = 'xfrxnnaxhtwxzoab'
---
+++ send_email.py
@@ -0,0 +1,38 @@
+from re import sub
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+import config
+
+def send_email_with_attach(destination=None, subject=None, body=None, attach_path_list=[]):
+    # Inicia o email
+    mimemsg = MIMEMultipart()
+    mimemsg['From']=config.email
+    mimemsg['To']=destination
+    mimemsg['Subject']=subject
+    if body:
+        mimemsg.attach(MIMEText(body, 'plain'))
+
+    # Para cada attachment na lista
+    for mail_attachment in attach_path_list:
+        mail_attachment_name = mail_attachment.split('/')[-1]
+
+        with open(mail_attachment, "rb") as attachment:
+            mimefile = MIMEBase('application', 'octet-stream')
+            mimefile.set_payload((attachment).read())
+            encoders.encode_base64(mimefile)
+            mimefile.add_header('Content-Disposition', "attachment; filename= %s" % mail_attachment_name)
+            mimemsg.attach(mimefile)
+
+    # Envia o email por o server smpt configurado em config
+    with smtplib.SMTP(config.host, config.port) as server:
+        server.starttls()
+        server.login(config.email, config.password)
+        server.send_message(mimemsg)
+
+if __name__ == '__main__':
+    # Para prueba
+    send_email_with_attach('artur.temporal@hotmail.com', 'Test', 'Test', ['/home/bcc/atc19/Captura de tela de 2022-10-26 14-18-28.png'])
